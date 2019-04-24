c5d4bc69aed4c2b9418f8353948d65bcbcf0a725
Piotr Kozłowski <piotr.kozlowski@miquido.com>
2019-04-24 19:53:39 +0200

    Implemented alpha version of introduction swipable pages screen.

---
+++ Countries/Intro/IntroPageCell.swift
@@ -0,0 +1,61 @@
+//
+//  IntroPageCell.swift
+//  Countries
+//
+//  Created by Piotr Kozłowski on 24/04/2019.
+//  Copyright © 2019 Piotr Kozłowski. All rights reserved.
+//
+
+import UIKit
+
+class IntroPageCell: UICollectionViewCell {
+    
+    var model: IntroPageModel? {
+        didSet {
+            guard let unwrappedModel = model else { return }
+            headerImageView.image = UIImage(named: unwrappedModel.imageName)
+            descriptionLabel.text = unwrappedModel.descriptionText
+        }
+    }
+    
+    private let headerImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "globe"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func initLayout() {
+        addSubview(headerImageView)
+        NSLayoutConstraint.activate([
+            headerImageView.topAnchor.constraint(equalTo: topAnchor, constant: 100),
+            headerImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            headerImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            headerImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            headerImageView.heightAnchor.constraint(equalToConstant: 250)])
+        
+        addSubview(descriptionLabel)
+        NSLayoutConstraint.activate([
+            descriptionLabel.topAnchor.constraint(equalTo: headerImageView.bottomAnchor, constant: 20),
+            descriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            descriptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            descriptionLabel.centerXAnchor.constraint(equalTo: centerXAnchor)])
+    }
+}
--- Countries/Intro/IntroViewController.swift
+++ Countries/Intro/IntroViewController.swift
@@ -8,13 +8,54 @@
 
 import UIKit
 
-class IntroViewController: UIViewController {
+class IntroViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
+    let pages = [
+        IntroPageModel(imageName: "globe", descriptionText: "First page"),
+        IntroPageModel(imageName: "globe", descriptionText: "Second page"),
+        IntroPageModel(imageName: "globe", descriptionText: "Third page"),
+        IntroPageModel(imageName: "globe", descriptionText: "Fourth page")
+    ]
+    
+    init() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        super.init(collectionViewLayout: layout)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        initCollectionView()
     }
 
-
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! IntroPageCell
+        
+        cell.model = pages[indexPath.item]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
+    
+    private func initCollectionView() {
+        collectionView?.backgroundColor = .white
+        collectionView?.isPagingEnabled = true
+        collectionView?.register(IntroPageCell.self, forCellWithReuseIdentifier: "cell")
+    }
+    
 }
 

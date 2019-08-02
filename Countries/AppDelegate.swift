536f81463fd7d714075aea92df972fd77a3271c8
Piotrek Kozłowski <piotr.kozlowski.1993@gmail.com>
2019-08-03 00:50:24 +0200

    Revert "Develop -> Master (#6)"
    This reverts commit 4c529580bedab01b10dac58a5ac4e7e642ee2f9b.

--- Countries/AppDelegate.swift
+++ Countries/AppDelegate.swift
@@ -16,16 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-    
-        let viewController: UIViewController
-        
-        if (DefaultsRepository().wasIntroductionDisplayed()) {
-            viewController = TabBarController()
-        } else {
-            viewController = IntroViewController()
-        }
-        
-        window!.rootViewController = viewController
+        let introViewController = IntroViewController()
+        window!.rootViewController = introViewController
         window!.makeKeyAndVisible()
         return true
     }
